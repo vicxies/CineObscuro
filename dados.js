b4ab91904228be06020deb92bd7abc27ece81b0d
vicxies <victorgpcamponogara@gmail.com>
2024-09-07 23:51:43 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -31,6 +31,46 @@ let dados = [
         links: ["https://letterboxd.com/ajschlecht/list/supernatural-horror/", 
             "https://pt.wikipedia.org/wiki/Filme_de_terror_sobrenatural"],
        tags: "fantasma, demônio, possessão, maldição, sobrenatural, oculto, assombração"
+    },
+    {
+        titulo: "Terror de Monstros",
+        tema: "Criaturas",
+        arma: ["Dentes afiados", "Garras", "Venenos"],
+        metodo: ["Ataque físico", "Transformação", "Invasão"],
+        ambiente: ["Floresta", "Espaço", "Cidade isolada"],
+        descricao: "Filmes de terror de monstros exploram a existência de criaturas sobrenaturais ou mutantes que aterrorizam a humanidade. As criaturas podem ser baseadas em mitos, lendas ou em conceitos científicos.",
+        links: ["https://letterboxd.com/serenditpity/list/horror-sub-genres-monster-horror/", "https://pt.wikipedia.org/wiki/Monstro"],
+        tags: "monstro, criatura, alienígena, mutante, zumbi, vampiro, lobisomem"
+    },
+    {
+        titulo: "Terror Found Footage",
+        tema: "Achado",
+        arma: ["Câmera", "Gravador"],
+        metodo: ["Documentário falso", "Encontro casual", "Evidência"],
+        ambiente: ["Casa assombrada", "Floresta","Local isolado"],
+        descricao: "O terror found footage simula um documentário encontrado por acaso, apresentando imagens reais e não editadas de eventos sobrenaturais ou horríveis. A sensação de autenticidade aumenta a imersão do espectador.",
+        links: ["https://letterboxd.com/horrorville/list/found-footage-horror-movies-ranked/", "https://pt.wikipedia.org/wiki/Found_footage"],
+        tags: "foundfootage, documentário, câmera, realista, sobrenatural"
+    },
+    {
+        titulo: "Terrir",
+        tema: "Humor negro",
+        arma: ["Ironia", "Exagero", "Situações absurdas"],
+        metodo: ["Sátira", "Paródia", "Comédia negra"],
+        ambiente: ["Dia a dia", "Situações extremas"],
+        descricao: "O terrir combina elementos de terror com comédia, criando uma experiência única que mistura medo e riso. O humor negro é utilizado para aliviar a tensão e satirizar os clichês do gênero terror.",
+        links: ["https://letterboxd.com/christianw18/list/letterboxds-top-50-horror-comedies/", "https://pt.wikipedia.org/wiki/Comédia_cinematográfica"],
+        tags: "terrir, comédia, humor negro, sátira, paródia"
+    },
+    {
+        titulo: "Terror Corporal",
+        tema: "Corpo humano",
+        arma: ["Doenças", "Mutações", "Transformações"],
+        metodo: ["Deformação", "Desfiguração", "Horror biológico"],
+        ambiente: "Corpo humano",
+        descricao: "O terror corporal explora as transformações e deformações do corpo humano, causando repulsa e desconforto no espectador. As doenças, mutações e experimentos científicos são temas comuns.",
+        links: ["https://letterboxd.com/burning_dog/list/body-horror-films/", "https://pt.wikipedia.org/wiki/Body_horror"],
+        tags: "bodyhorror, corpo, mutação, deformação, biológico, doença"
     }
 ];
 
